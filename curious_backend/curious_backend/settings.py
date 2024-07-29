2113647d9925d8762cfd51b0cf0c22baaf953b1a
syamshukla <53063487+syamshukla@users.noreply.github.com>
2024-07-28 21:39:25 -0400

    db change

--- curious_backend/curious_backend/settings.py
+++ curious_backend/curious_backend/settings.py
@@ -76,14 +76,23 @@ WSGI_APPLICATION = 'curious_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'curiousFacts',
+        'USER': 'admin',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': ''
     }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
