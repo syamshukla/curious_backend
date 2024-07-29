d3931bc5299616a56945c47435814e6138552b60
syamshukla <53063487+syamshukla@users.noreply.github.com>
2024-07-28 22:14:15 -0400

    testing

--- curious_backend/curious_backend/settings.py
+++ curious_backend/curious_backend/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'curious_backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'curiousFacts',
+        'NAME': 'curiousfacts',
         'USER': 'admin',
         'PASSWORD': 'password',
         'HOST': 'localhost',
